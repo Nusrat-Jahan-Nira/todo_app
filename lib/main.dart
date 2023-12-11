86598a22caa85660f27cae007ebb989eea7a15e5
nusrat <nusrat.jahan.nira@outlook.com>
2023-12-11 17:51:57 +0600

    add todo added and design changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:todo_app/todo_page.dart';
 
 void main() {
@@ -14,6 +15,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        useMaterial3: true,
+
+
+        // Define the default brightness and colors.
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          // ···
+          // brightness: Brightness.dark,
+        ),
+
+        buttonTheme: ButtonThemeData(
+          shape: const LinearBorder(),
+          buttonColor: Colors.green,
+          colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.green,
+        ),
+        ),
+        // Define the default `TextTheme`. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold
+          ),
+          // ···
+          titleLarge: GoogleFonts.pacifico(
+            fontSize: 26
+          ),
+          bodyMedium: GoogleFonts.merriweather(
+          ),
+          displaySmall: GoogleFonts.pacifico(
+
+          ),
+        ),
         appBarTheme: const AppBarTheme(
           color: Colors.blueAccent,
         ),
--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:todo_app/enum_value.dart';
 import 'package:todo_app/my_todo.dart';
@@ -12,8 +11,8 @@ class TodoPage extends StatefulWidget {
 }
 
 class _TodoPageState extends State<TodoPage> {
-
-
+  final textController = TextEditingController();
+  TodoPriority todoPriority = TodoPriority.Normal;
 
   @override
   Widget build(BuildContext context) {
@@ -21,96 +20,116 @@ class _TodoPageState extends State<TodoPage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _settingModalBottomSheet(context);
+          addTodo();
+          // _settingModalBottomSheet(context);
         },
         child: const Icon(Icons.add),
       ),
       appBar: AppBar(
-        title: const Text('Todo App'),),
-      body:MyTodo.todos.isEmpty?
-      const Center(child: Text('Nothing to do!'),):
-      ListView.builder(
-        itemCount: MyTodo.todos.length,
-          itemBuilder: (context,index){
-            final todos = MyTodo.todos[index];
-            return TodoItem(todo: todos, onChanged: (value){
-
-              setState(() {
-                MyTodo.todos[index].completed = value;
-              });
-
-            });
-          }
+        title: const Text('Todo App'),
       ),
+      body: MyTodo.todos.isEmpty
+          ? const Center(
+              child: Text('Nothing to do!'),
+            )
+          : ListView.builder(
+              itemCount: MyTodo.todos.length,
+              itemBuilder: (context, index) {
+                final todos = MyTodo.todos[index];
+                return TodoItem(
+                    todo: todos,
+                    onChanged: (value) {
+                      setState(() {
+                        MyTodo.todos[index].completed = value;
+                      });
+                    });
+              }),
     );
   }
-}
-  void _settingModalBottomSheet(context){
-    final textController = TextEditingController();
 
+  void addTodo() {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
-        builder: (BuildContext bc){
-          return Wrap(
-            children: <Widget>[
-               ListTile(
-                  leading:  const Icon(Icons.note_add_outlined),
-                  title:  TextField(
-                    controller: textController,
-                    decoration:  const InputDecoration(
+        builder: (context) => StatefulBuilder(
+          builder:(context, setBuilderState) => Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                  children: [
+                    TextField(
+                      controller: textController,
+                      decoration: const InputDecoration(
                         label: Text('What to do?'),
+                      ),
                     ),
-                  ),
-                  onTap: () => {}
-              ),
-               ListTile(
-                title:  const Center(child: Text('Select Priority')),
-                onTap: () => {},
-              ),
-
-              Row(
-                children: [
-                  Radio<TodoPriority>(
-                      value: TodoPriority.Low,
-                      groupValue: null,
-                      onChanged: (value) {
-
-                      }),
-                  const Text('Low'),
-                  Radio<TodoPriority>(
-                      value: TodoPriority.Normal,
-                      groupValue: null,
-                      onChanged: (value) {
-
-                      }),
-                  const Text('Normal'),
-                  Radio<TodoPriority>(
-                      value: TodoPriority.High,
-                      groupValue: null,
-                      onChanged: (value) {
-
-                      }),
-                  const Text('High'),
-                ],
-              ),
-
-              Center(
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      textStyle: const TextStyle(
-                        color: Colors.white
-                      )
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('Select Priority'),
                     ),
-                    onPressed: () {
 
-                },
-                    child: const Text('SAVE')),
-              )
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Radio<TodoPriority>(
+                            value: TodoPriority.Low,
+                            groupValue: todoPriority,
+                            onChanged: (value){
+                              setBuilderState(() {
+                                todoPriority = value!;
+                              });
+                            }),
+                        Text(TodoPriority.Low.name),
+                        Radio<TodoPriority>(
+                            value: TodoPriority.Normal,
+                            groupValue: todoPriority,
+                            onChanged: (value){
+                              setBuilderState(() {
+                                todoPriority = value!;
+                              });
+                            }),
+                        Text(TodoPriority.Normal.name),
+                        Radio<TodoPriority>(
+                            value: TodoPriority.High,
+                            groupValue: todoPriority,
+                            onChanged: (value){
+                              setBuilderState(() {
+                                todoPriority = value!;
+                              });
+                            }),
+                        Text(TodoPriority.High.name),
 
-            ],
-          );
-        }
-    );
+                      ],
+                    ),
 
+                    ElevatedButton(
+                        onPressed: (){
+                          _save();
+                        },
+                        child: const Text('SAVE')
+                    )
+                  ],
+                ),
+          ),
+        ));
+  }
+
+  void _save() {
+     if(textController.text.isEmpty){
+       showMsg(context,'Input field must not be empty');
+       return;
+     }
+  }
 }
+
+void showMsg(BuildContext context, String s) {
+
+  showDialog(context: context, builder: (context)=>AlertDialog(
+    title: const Text('Caution!'),
+    content: Text(s),
+    actions: [
+      TextButton(onPressed: ()=> Navigator.pop(context),
+          child: const Text('CLOSE'))
+    ],
+  ));
+}
+
